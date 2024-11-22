0a05e9a8576491d2d47f80e1e4f6276be928323c
ST10301125 <129761629+ST10301125@users.noreply.github.com>
2024-11-22 23:26:03 +0200

    Did Authorisation for users

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddSession(options =>
 
 // Add controllers, views, and Razor pages
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(); 
 
 // Build the app
 var app = builder.Build();
